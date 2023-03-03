c63c4f09fe97e617babe61f9dd480b52be8fab7f
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-03 21:15:47 +0530

    Ability to create linked list by Adding

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@
         public static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
+            list.AddInReverseOrder(70);
+            list.AddInReverseOrder(30);
+            list.AddInReverseOrder(56);
             list.Display();
         }
     }
