0e782f6763010cb4a41ad0dd9b87a2c0373c56d8
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-03 21:07:16 +0530

    Create simple Linked List of 56,30,70

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to data structure Problem");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
