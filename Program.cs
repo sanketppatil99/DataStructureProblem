46541356ed795815aaa73b447e3fe52043213582
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-03 21:00:12 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructure
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure Problem");
+        }
+    }
+}
